65cebeb285e17afa5d1851961bea97cd62da46f1
Kemar <120325211+revenge0428@users.noreply.github.com>
2024-08-16 16:28:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function scrollCarousel(direction) {
     behavior: 'smooth'
   });
 }
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
